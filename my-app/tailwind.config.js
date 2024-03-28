838368e8de93c8e1f8d8da59b314c8667feb3c20
StringSlayyer <samuel.zarsky@gmail.com>
2024-03-29 00:58:28 +0100

    update

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,16 +1,30 @@
-import {nextui} from '@nextui-org/react'
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      black: {
+        200: "#707070",
+        300: "#1E1E1E",
+      },
+      grey: {
+        50: "#B9B9B9",
+        200: "#606060",
+      },
+    },
     extend: {},
+    darkMode: "class",
   },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+  plugins: [
+    nextui({
+      addCommonColors: true,
+    }),
+  ],
+};
